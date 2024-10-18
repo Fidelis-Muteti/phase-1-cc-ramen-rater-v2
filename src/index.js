c662a37c156aba5656036a931a2b1926f80747ef
Fidelis-Muteti <fidelmwovi@gmail.com>
2024-10-18 23:31:46 +0300

    ramen

--- src/index.js
+++ src/index.js
@@ -1,82 +1,76 @@
-const ramenMenuDiv = document.getElementById("ramen-menu")
-const newRamenData = document.getElementById("new-ramen")
-document.getElementById("new-ramen").addEventListener("submit",newRamen)
-fetch("http://localhost:3000/ramens")
-     .then(response => response.json())
-     .then(data => {
-      data.forEach(ramen => {
-      
-        displayRamen(ramen);
-      })
-})
-.catch((err) => console.log(err));
+const ramenMenuDiv = document.getElementById("ramen-menu");
+const newRamenData = document.getElementById("new-ramen");
+const baseUrl = "http://localhost:3000/ramens";
 
-function displayRamen(ramen){
+document.getElementById("new-ramen").addEventListener("submit", newRamen);
+
+
+fetch(baseUrl)
+  .then((response) => response.json())
+  .then((data) => {
+    data.forEach((ramen) => {
+      displayRamen(ramen);
+    });
+  })
+  .catch((err) => console.log(err));
+
+
+function displayRamen(ramen) {
   const ramenImageElement = document.createElement("img");
   ramenImageElement.src = ramen.image;
   ramenMenuDiv.append(ramenImageElement);
 
-  ramenImageElement.addEventListener("click",() => clickHand(ramen))
+  ramenImageElement.addEventListener("click", () => clickHand(ramen)); 
 }
-
 
 
 function clickHand(ramen) {
   document.querySelector(".detail-image").src = ramen.image;
-  document.querySelector(".detail-image").alt = ramen.name;  
-  document.getElementById("name").textContent = ramen.name;
-  document.getElementById("restaurant").textContent = ramen.restaurant;
+  document.querySelector(".detail-image").alt = ramen.name;
+  document.querySelector(".name").textContent = ramen.name; 
+  document.querySelector(".restaurant").textContent = ramen.restaurant; 
   document.getElementById("rating-display").textContent = ramen.rating;
   document.getElementById("comment-display").textContent = ramen.comment;
 }
 
+
 function newRamen(e) {
-     e.preventDefault();
-        
-          const newRamenData = {
-           name: e.target.name.value,
-           restaurant: e.target.restaurant.value,
-           image: e.target.image.value,
-           rating: e.target.rating.value,
-           comment: e.target["new-comment"].value
-          };
-     displayRamen(newRamenData) 
-     e.target.reset(); 
-  }
+  e.preventDefault(); 
 
+  const newRamenData = {
+    name: e.target.name.value,
+    restaurant: e.target.restaurant.value,
+    image: e.target.image.value,
+    rating: parseInt(e.target.rating.value), 
+    comment: e.target["new-comment"].value,
+  };
 
+ 
+  fetch(baseUrl, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(newRamenData),
+  })
+    .then((res) => res.json())
+    .then((data) => {
 
-function el(id) {
-  return document.getElementById(id);
+      displayRamen(data);
+      alert(`${data.name} created successfully!`);
+    })
+    .catch((err) => console.log(err));
+
+  e.target.reset(); 
 }
 
 
-    
-        
-        // Callbacks
-const handleClick = (ramen) => {
-  // Add code
-};
-
-const addSubmitListener = () => {
-  // Add code
+function main() {
+  addSubmitListener(); 
 }
 
-const displayRamens = () => {
-  // Add code
-};
-
-const main = () => {
-  displayRamen()// Invoke displayRamens here
-  addSubmitListener()// Invoke addSubmitListener here
+function addSubmitListener() {
+  newRamenData.addEventListener("submit", newRamen);
 }
 
-main()
-
-document.addEventListener("DOMContentLoaded", main);// Export functions for testing
-export {
-  displayRamens,
-  addSubmitListener,
-  handleClick,
-  main,
-};
+document.addEventListener("DOMContentLoaded", main);
